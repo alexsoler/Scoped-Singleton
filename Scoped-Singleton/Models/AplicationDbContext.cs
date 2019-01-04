08fe23866b423028f082fb0c606b53264e53323e
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-01-04 11:25:05 -0600

    Creacion del DbContext para el acceso a la base de datos

---
+++ Scoped-Singleton/Models/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scoped_Singleton.Models
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Registro> Registros { get; set; }
+    }
+}
