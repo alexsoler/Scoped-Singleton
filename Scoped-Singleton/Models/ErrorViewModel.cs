b457095e61fcaa5f7288bb9e0bc7052f2365f496
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-01-04 11:08:00 -0600

    Agregar archivos de proyecto.

---
+++ Scoped-Singleton/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Scoped_Singleton.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
